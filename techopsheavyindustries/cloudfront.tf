89d10cc1034cc088c6fc0f984955b46c4937c367
Cory Farinella <4465326+el-dude@users.noreply.github.com>
2023-03-03 22:31:56 -0500

    updated custom error's for cache

--- techopsheavyindustries/cloudfront.tf
+++ techopsheavyindustries/cloudfront.tf
@@ -2,7 +2,7 @@
 resource "aws_cloudfront_distribution" "root_s3_distribution" {
   origin {
     domain_name               = aws_s3_bucket.root_bucket.bucket_regional_domain_name
-    origin_id                 = "S3-.${var.bucket_name}"
+    origin_id                 = "S3-${var.bucket_name}"
     custom_origin_config {
       http_port               = 80
       https_port              = 443
@@ -15,11 +15,12 @@ resource "aws_cloudfront_distribution" "root_s3_distribution" {
   is_ipv6_enabled             = true
 
   aliases                     = [var.domain_name]
+  default_root_object         = "index.html"
 
   default_cache_behavior {
     allowed_methods           = ["GET", "HEAD"]
     cached_methods            = ["GET", "HEAD"]
-    target_origin_id          = "S3-.${var.bucket_name}"
+    target_origin_id          = "S3-${var.bucket_name}"
 
     forwarded_values {
       query_string            = true
@@ -31,10 +32,77 @@ resource "aws_cloudfront_distribution" "root_s3_distribution" {
       headers                 = ["Origin"]
     }
 
-    viewer_protocol_policy    = "allow-all"
+    viewer_protocol_policy    = "redirect-to-https"
     min_ttl                   = 0
     default_ttl               = 86400
     max_ttl                   = 31536000
+  }
+
+  custom_error_response {
+    error_code                = 400
+    response_code             = 400
+    error_caching_min_ttl     = 300
+    response_page_path        = "/error.html"
+  }
+  custom_error_response {
+    error_code                = 403
+    response_code             = 403
+    error_caching_min_ttl     = 300
+    response_page_path        = "/error.html"
+  }
+  custom_error_response {
+    error_code                = 404
+    response_code             = 404
+    error_caching_min_ttl     = 300
+    response_page_path        = "/404.html"
+  }
+  custom_error_response {
+    error_code                = 405
+    response_code             = 405
+    error_caching_min_ttl     = 300
+    response_page_path        = "/error.html"
+  }
+  custom_error_response {
+    error_code                = 414
+    response_code             = 414
+    error_caching_min_ttl     = 300
+    response_page_path        = "/error.html"
+  }
+  custom_error_response {
+    error_code                = 416
+    response_code             = 416
+    error_caching_min_ttl     = 300
+    response_page_path        = "/error.html"
+  }
+  custom_error_response {
+    error_code                = 500
+    response_code             = 500
+    error_caching_min_ttl     = 300
+    response_page_path        = "/error.html"
+  }
+  custom_error_response {
+    error_code                = 501
+    response_code             = 501
+    error_caching_min_ttl     = 300
+    response_page_path        = "/error.html"
+  }
+  custom_error_response {
+    error_code                = 502
+    response_code             = 502
+    error_caching_min_ttl     = 300
+    response_page_path        = "/error.html"
+  }
+  custom_error_response {
+    error_code                = 503
+    response_code             = 503
+    error_caching_min_ttl     = 300
+    response_page_path        = "/error.html"
+  }
+  custom_error_response {
+    error_code                = 504
+    response_code             = 504
+    error_caching_min_ttl     = 300
+    response_page_path        = "/error.html"
   }
 
   restrictions {
